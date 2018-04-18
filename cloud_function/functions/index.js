eb08063379a0a2eb054fa7adf63dfce0528720da
Edgar Amores <max1285@gmail.com>
2018-04-17 23:43:51 -0400

    firebase cloud function

---
+++ cloud_function/functions/index.js
@@ -0,0 +1,24 @@
+const functions = require('firebase-functions');
+
+const admin = require('firebase-admin');
+admin.initializeApp();
+
+const DialogflowApp = require('actions-on-google').DialogflowApp;
+
+exports.receiveAssistantRequests = functions.https.onRequest((request, response) => {
+
+    const app = new DialogflowApp({request: request, response: response});
+
+    function handlerRequest(app) {
+
+        const device = app.getArgument('devices');
+        const status = app.getArgument('status');
+
+        return admin.database().ref(`/automation/${device}/value`).set(status)
+            .then(snapshot => {
+                app.ask(`Ok, switching ${device} ${status}. Do you want to control anything else?`);
+            });
+
+    }
+    app.handleRequest(handlerRequest);
+});
